7016bfcc0efa1890285e83b508ceef778821ac8d
Jyoti Naik <jyotinaik99@gmail.com>
2025-02-03 12:51:35 +0530

    github pages package json

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import './index.css';
 import { ThemeProvider } from './common/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
 );
